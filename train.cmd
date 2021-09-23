8a21f3bd5881174f96f05bac1e788a9d63133210
Enrico <enrico.detoma@gmail.com>
2021-09-23 09:56:08 +0200

    -

--- train.cmd
+++ train.cmd
@@ -1,3 +1,4 @@
-mkdir lmdb
-python prepare_data.py --out lmdb --n_worker 16 /Users/Enrico/Downloads/stylegan2-pytorch/modern_paintings_resized/
-rem python -m torch.distributed.launch --nproc_per_node=2 --master_port=9876 train.py --arch swagan /Users/Enrico/Downloads/stylegan2-pytorch/lmdb
+set LDMB_PATH=d:\lmdb
+mkdir %LDMB_PATH%
+python prepare_data.py --out %LDMB_PATH% --n_worker 16 ./modern_paintings_resized/
+rem python -m torch.distributed.launch --nproc_per_node=2 --master_port=9876 train.py --arch swagan %LDMB_PATH%
